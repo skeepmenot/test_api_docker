6275a33b4567221c6443de1d4ec47eeb928dcd06
Harin3Bone <harin.thananam@gmail.com>
2022-05-06 15:32:43 +0700

    Update project configuration

---
+++ BackEnd/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS public.todolist
+(
+    id                 varchar(36)  DEFAULT ''    NOT NULL  CONSTRAINT todo PRIMARY KEY,
+    topic              varchar(150) DEFAULT ''    NOT NULL,
+    description        varchar(300) DEFAULT ''    NOT NULL,
+    created_timestamp  timestamptz  DEFAULT now() NOT NULL,
+    modified_timestamp timestamptz  DEFAULT now() NOT NULL,
+    is_check           boolean      DEFAULT false NOT NULL
+    );
+
+CREATE UNIQUE INDEX todolist_id_uindex ON public.todolist (id);
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
