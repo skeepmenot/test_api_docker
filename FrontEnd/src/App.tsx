d8b477fbd258326551d791a61e0a3d869ed31c0e
pawaox4th <pawaox4th@gmail.com>
2022-05-07 22:28:44 +0700

    feat: 🎸 Create from create todo app

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import { useState } from "react"
 import Container from "./components/Container"
 import logo from "./logo.svg"
@@ -5,38 +6,76 @@ import logo from "./logo.svg"
 function App() {
   return (
     <div className="App  min-h-screen w-full bg-neutral-100">
-      <Container className="w-5/12 sm:pt-8">
+      <Container className="w-10/12 pt-5 sm:w-5/12 sm:pt-8">
         <h1 className="font-noto-sarif font-bold text-light---primary-color">
           Todo App.
         </h1>
-        <p className="mt-2 text-base">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          corrupti nemo harum quidem commodi incidunt pariatur odit ipsa, minima
-          reprehenderit sunt delectus aspernatur numquam consectetur libero
-          illum nam, doloribus sint culpa deserunt quisquam! Mollitia eum
-          voluptas reiciendis nisi, quam assumenda explicabo obcaecati
-          laboriosam voluptates delectus dignissimos impedit sequi ullam
-          quibusdam nihil, enim consequatur aliquid, ab tenetur excepturi
-          sapiente. Veritatis, corporis? Veniam placeat odio in. Officiis
-          aperiam velit, error temporibus quasi quibusdam repellendus ea
-          voluptatum blanditiis, eum voluptates cumque amet nisi sit magni
-          minima corporis maiores hic nam doloremque asperiores, molestiae
-          pariatur. Deserunt quisquam voluptas quaerat ut architecto odio vitae
-          animi.
-        </p>
-        <div>
-          <form>
-            <label htmlFor="todo-title">
+        <div className="mt-5">
+          <form className="grid grid-flow-row grid-cols-3 gap-y-4 gap-x-4">
+            <label htmlFor="todo-title" className="col-span-3 ">
               <input
                 id="todo-title"
                 type="text"
                 spellCheck
                 required
-                placeholder="Please type title todo hear."
-                className={`px-2 py-3 focus:outline-light---primary-hover-color`}
+                placeholder="Please type title todo here."
+                className={`w-full px-2 py-3 focus:outline-light---primary-hover-color  sm:w-60 `}
               />
             </label>
+            <label htmlFor="" className="col-span-3">
+              <textarea
+                name=""
+                id=""
+                cols={30}
+                rows={10}
+                required
+                className={`w-full p-4 focus:outline-light---primary-hover-color`}
+                placeholder="Please type content here."></textarea>
+            </label>
+            <button
+              className={clsx(
+                "md:col-start-2 md:col-end-2",
+                "col-span-3",
+                "w-full",
+                "rounded-md",
+                "border",
+                "border-light---negative-color",
+                "hover:border-light---negative-color-hover",
+                "bg-white",
+                "px-4",
+                "py-2",
+                "text-light---negative-color",
+                "hover:text-light---negative-color-hover",
+                "disabled:text-light---disabled-text-color",
+                "disabled:bg-light---disabled-background-color",
+                "disabled:border-neutral-200",
+                "disabled:cursor-not-allowed",
+                "hover:shadow-md",
+                "hover:shadow-light---negative-color/30"
+              )}>
+              Cancle
+            </button>
+            <button
+              className={clsx(
+                "md:col-start-3 md:col-end-3",
+                "col-span-3",
+                "w-full",
+                "px-4",
+                "py-2",
+                "rounded-md",
+                "bg-light---primary-color",
+                "hover:bg-light---primary-hover-color",
+                "hover:shadow-md",
+                "hover:shadow-light---primary-color/40",
+                "text-white",
+                "hover:text-light---text-color-on-primary",
+                "disabled:text-light---disabled-text-color",
+                "disabled:bg-light---disabled-background-color",
+                "disabled:border-neutral-300",
+                "disabled:cursor-not-allowed"
+              )}>
+              Add todo.
+            </button>
           </form>
         </div>
       </Container>
